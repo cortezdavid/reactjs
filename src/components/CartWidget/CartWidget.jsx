b659dfde5ca4fc560ce7b8487f7b79e156907722
cortezdavid <david.cortez020@gmail.com>
2021-12-03 22:47:18 -0300

    dandole estilo al navbar

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { FaShoppingCart } from 'react-icons/fa';
+import { BsCart3 } from 'react-icons/bs';
 import { useCartContext } from '../../context/CartContext'
 import './CartWidget.css';
 
 
 const CartWidget = () => {
 
-    const {quantityItems} = useCartContext()
+    const { quantityItems } = useCartContext()
 
     return (
         <div className='cart position-relative'>
-            <FaShoppingCart />
+            <BsCart3 />
             <span className="position-absolute start-100 translate-middle badge rounded-pill bg-danger">
                 {quantityItems() !== 0 && quantityItems()}
             </span>   
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,27 @@
 import './NavBar.css';
 import logo from './logo.png'
 import CartWidget from '../CartWidget/CartWidget';
-import {NavDropdown} from 'react-bootstrap'
+import {NavDropdown, Navbar, Nav, Container} from 'react-bootstrap'
 import {Link, NavLink} from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <nav>
-            <div className="containerNav">
+        <Navbar bg="light" expand="lg" className='fixed-top'>
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Link to='/'><img src= {logo} alt="" className='logo'/></Link>
-                <ul>
-                    <li><Link to='/'>Inicio</Link></li>
-                    <li>
-                        <NavDropdown title="Categoría" id="collasible-nav-dropdown">
-                            <NavDropdown.Item as={NavLink} to='/category/DC'>DC</NavDropdown.Item>
-                            <NavDropdown.Item as={NavLink} to='/category/Marvel'>Marvel</NavDropdown.Item>
-                        </NavDropdown>
-                    </li>
-                </ul>
-                <Link to='/cart' className='cart'><CartWidget /></Link>
-            </div>
-            
-        </nav>
+                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+                <Nav>
+                    <Nav.Link as={ NavLink } to='/'>Inicio</Nav.Link>
+                    <NavDropdown title="Categoria" id="basic-nav-dropdown">
+                        <NavDropdown.Item as={ NavLink } to='/category/DC'>DC</NavDropdown.Item>
+                        <NavDropdown.Item as={ NavLink } to='/category/Marvel'>Marvel</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+                <Link to='/cart'><CartWidget /></Link>
+            </Container>
+        </Navbar>
     )
 }
 
