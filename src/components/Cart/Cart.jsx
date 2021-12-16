9949db49a56584d219344f59e70bce263838500e
cortezdavid <david.cortez020@gmail.com>
2021-12-16 19:09:45 -0300

    estilos al cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useCartContext } from '../../context/CartContext'
 import {Table} from 'react-bootstrap'
-import { FaRegTimesCircle } from "react-icons/fa";
+import { BsXLg } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 import './Cart.css'
@@ -15,35 +15,39 @@ const Cart = () => {
             {cartList.length === 0 ? <h1>Carrito Vacio</h1> : 
             <>
             <h1>Carrito</h1>
-            <Table striped bordered hover className='container'>
-                <thead>
-                    <tr>
-                    <th>nombre</th>
-                    <th>cantidad</th>
-                    <th>precio</th>
-                    <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cartList.map(product =>
-                    <tr key={product.id}>
-                        <th>{product.title}</th>
-                        <th>{product.quantity}</th>
-                        <th>${product.price}</th>
-                        <th><button><FaRegTimesCircle onClick={() => {deleteItem(product.id)}} /></button></th>
-                    </tr>)}
-                </tbody>
-                <tfoot>
-                    <tr>
+            <div className="asd">
+                <Table striped bordered hover className='container'>
+                    <thead>
+                        <tr>
+                        <th>Nombre</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
                         <th></th>
-                        <th>PRECIO TOTAL:</th>
-                        <th>${totalPrice()}</th>
-                    </tr>
-                </tfoot>
-            </Table>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cartList.map(product =>
+                        <tr key={product.id}>
+                            <th>{product.title}</th>
+                            <th>{product.quantity}</th>
+                            <th>${product.price}</th>
+                            <th><button className="btn btn-danger"><BsXLg onClick={() => {deleteItem(product.id)}} /></button></th>
+                        </tr>)}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th></th>
+                            <th>PRECIO TOTAL:</th>
+                            <th>${totalPrice()}</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+                </Table>
+            </div>
+            
             <div className='containerButtons d-flex justify-content-center'>
                 <div className='buttons d-flex justify-content-between'>
-                    <Link to='/form'><button className='btn btn-secondary'>Checkout</button></Link>
+                    <Link to='/form'><button className='btn btn-secondary'>Comprar</button></Link>
                     <button className='btn btn-danger' onClick={clear}>Borrar todo</button>
                 </div>
             </div>
